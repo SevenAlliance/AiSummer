58a5e7fe54d64f9c094e9b779705c2428316dc53
Arvin Yan <fkwy0516@163.com>
2020-11-15 16:28:35 +0800

    Summer 20201115
    Summer 20201115

---
+++ Summer/src/Summer.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author yshic
+ *
+ */
+public class Summer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
