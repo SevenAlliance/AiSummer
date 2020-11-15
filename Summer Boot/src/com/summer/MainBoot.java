ca4b02b103cd87d5cf51dfcbd4414f94f2e39254
Arvin Yan <fkwy0516@163.com>
2020-11-15 16:31:40 +0800

    Summer Creat  20201115
    Summer Creat  20201115

---
+++ Summer Boot/src/com/summer/MainBoot.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.summer;
+
+/**
+ * @author yshic
+ *
+ */
+public class MainBoot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
